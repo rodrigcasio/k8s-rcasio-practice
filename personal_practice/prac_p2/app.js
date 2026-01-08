948c7019715fbaa2c3c80a456609344af95fc74e
rodrigcasio <rod.casio@outlook.com>
2026-01-08 16:11:17 -0600

    `feat:` Set up application for new image

---
+++ personal_practice/prac_p2/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'public', 'index.html');
+
+  // read file 
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error loading page');
+    }
+
+    // replace palceholders with real values:
+    const message = process.env.APP_MESSAGE || "Welcome to Version 2.0";
+    const podName = os.localhost();
+    
+    let result = data.replace('{{MESSAGE}}', message)
+                     .replace('{{POD_NAME}}', podName);
+
+    res.send(result);
+
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App v2 listening at http://localhost:${PORT}`);
+})
