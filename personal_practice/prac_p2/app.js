4d731e1eed20082f0f0e6335617c20f573ee9707
rodrigcasio <rod.casio@outlook.com>
2026-01-08 21:03:29 -0600

    `fix:` Moved template within the root folder app

--- personal_practice/prac_p2/app.js
+++ personal_practice/prac_p2/app.js
@@ -6,21 +6,21 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
-app.use(express.static('public'));
+app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'public', 'index.html');
+
+  const filePath = path.join(__dirname, 'index.html');
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Error loading index.html');
+      return res.status(500).send('Error loading template');
     }
 
-    const message = process.env.APP_MESSAGE || "Welcome to Version 5.0";
+    const message = process.env.APP_MESSAGE || "Welcome to Version 6.0";
     const podName = os.hostname();
 
-    // USE replaceAll to ensure it finds the strings
     let result = data.replaceAll('{{MESSAGE}}', message);
     result = result.replaceAll('{{POD_NAME}}', podName);
 
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`App v5 listening at http://localhost:${PORT}`);
+  console.log(`App v6 listening at http://localhost:${PORT}`);
 });
