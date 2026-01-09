74eb99f53f24a1faa50f1a4968b21822ce4e41c6
rodrigcasio <rod.casio@outlook.com>
2026-01-08 20:36:05 -0600

    `fix:` Set up final v5 for correct app containerized and deployed in k8s

--- personal_practice/prac_p2/app.js
+++ personal_practice/prac_p2/app.js
@@ -6,29 +6,28 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
-app.use(express.static('public')); // find .css from public/ automatically
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'public', 'index.html');
 
-  // read file 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      return res.status(500).send('Error loading page');
+      console.error(err);
+      return res.status(500).send('Error loading index.html');
     }
 
-    // replace palceholders with real values:
-    const message = process.env.APP_MESSAGE || "Welcome to Version 4.0";
+    const message = process.env.APP_MESSAGE || "Welcome to Version 5.0";
     const podName = os.hostname();
-    
-    let result = data.replace('{{MESSAGE}}', message)
-                     .replace('{{POD_NAME}}', podName);
+
+    // USE replaceAll to ensure it finds the strings
+    let result = data.replaceAll('{{MESSAGE}}', message);
+    result = result.replaceAll('{{POD_NAME}}', podName);
 
     res.send(result);
-
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`App v4 listening at http://localhost:${PORT}`);
-})
+  console.log(`App v5 listening at http://localhost:${PORT}`);
+});
